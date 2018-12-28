9298027d250aa5b508223800d52b179a3bac9820
Aynasa <46222619+Aynasa@users.noreply.github.com>
2018-12-28 16:52:17 +0300

    Add files via upload

---
+++ BLL/Models/Client.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Models
+{
+    public class ClientMod
+    {
+        public int Id { get; set; }
+        public string FIO { get; set; }        
+        public string Passport { get; set; }
+      //  public List<int> TakenVkladsIds { get; set; }
+
+    }
+}
---
+++ BLL/Models/Vklad.cs
@@ -0,0 +1,32 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Models
+{
+    public class VkladMod
+    {
+
+        public int Balance { get; set; }
+
+        public DateTime DateOpen { get; set; }
+
+
+
+        public int Percent { get; set; }
+        public string Prog_name { get; set; }
+
+        public int ClientId { get; set; }
+        public int ProgId { get; set; }
+        public int Id { get; set; }
+
+
+
+
+
+
+    }
+}
